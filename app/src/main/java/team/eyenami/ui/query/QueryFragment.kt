8598212d135e9f748ae6589f763a2300a8cfeeea
duska <duskagk@gmail.com>
2024-07-19 00:17:43 +0900

    Test : 2024-07-18
      # 브랜치 테스트

--- app/src/main/java/team/eyenami/ui/query/QueryFragment.kt
+++ app/src/main/java/team/eyenami/ui/query/QueryFragment.kt
@@ -19,7 +19,7 @@ class QueryFragment : Fragment(R.layout.fragment_query) {
     private lateinit var chatAdapter: ChatAdapter
     private val generativeModel = GenerativeModel(
 //        modelName = "gemini-pro-vision",  // TODO 이미지 전송 샘플
-        modelName = "gemini-pro",  // 텍스트 전용 모델로 변경
+        modelName = "gemini-pro",  // 텍스트 전용 모델로 변경sdfsdfd
         apiKey = BuildConfig.apiKey
     )
 
